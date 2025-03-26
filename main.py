ef798ddc83916f7207b113680992d63461f63755
Igor Chertovskoy <igor.chertovskoy.88.skypro@gmail.com>
2025-03-26 05:50:42 +0300

    add_tests

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.widget import mask_account_card, get_date
 from src.processing import filter_by_state, sort_by_date
+from src.generators import card_number_generator
 
 if __name__ == '__main__':
     card_nums = [
@@ -27,11 +28,98 @@ if __name__ == '__main__':
     print(sort_by_date(data))
 
 if __name__ == 'main':
+    transactions = (
+        [
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+                "operationAmount": {
+                    "amount": "9824.07",
+                    "currency": {
+                        "name": "USD",
+                        "code": "USD"
+                    }
+                },
+                "description": "Перевод организации",
+                "from": "Счет 75106830613657916952",
+                "to": "Счет 11776614605963066702"
+            },
+            {
+                "id": 142264268,
+                "state": "EXECUTED",
+                "date": "2019-04-04T23:20:05.206878",
+                "operationAmount": {
+                    "amount": "79114.93",
+                    "currency": {
+                        "name": "USD",
+                        "code": "USD"
+                    }
+                },
+                "description": "Перевод со счета на счет",
+                "from": "Счет 19708645243227258542",
+                "to": "Счет 75651667383060284188"
+            },
+            {
+                "id": 873106923,
+                "state": "EXECUTED",
+                "date": "2019-03-23T01:09:46.296404",
+                "operationAmount": {
+                    "amount": "43318.34",
+                    "currency": {
+                        "name": "руб.",
+                        "code": "RUB"
+                    }
+                },
+                "description": "Перевод со счета на счет",
+                "from": "Счет 44812258784861134719",
+                "to": "Счет 74489636417521191160"
+            },
+            {
+                "id": 895315941,
+                "state": "EXECUTED",
+                "date": "2018-08-19T04:27:37.904916",
+                "operationAmount": {
+                    "amount": "56883.54",
+                    "currency": {
+                        "name": "USD",
+                        "code": "USD"
+                    }
+                },
+                "description": "Перевод с карты на карту",
+                "from": "Visa Classic 6831982476737658",
+                "to": "Visa Platinum 8990922113665229"
+            },
+            {
+                "id": 594226727,
+                "state": "CANCELED",
+                "date": "2018-09-12T21:27:25.241689",
+                "operationAmount": {
+                    "amount": "67314.70",
+                    "currency": {
+                        "name": "руб.",
+                        "code": "RUB"
+                    }
+                },
+                "description": "Перевод организации",
+                "from": "Visa Platinum 1246377376343588",
+                "to": "Счет 14211924144426031657"
+            }
+        ]
+    )
+
+
+if __name__ == 'main':
     assert get_date('2019-07-03T18:35:29.512364') == '03.07.2019'
 
     assert mask_account_card("Maestro 1596837868705199") == "Maestro 1596 83** **** 5199"
     assert mask_account_card("Счет 64686473678894779589") == "Счет **9589"
     assert mask_account_card("") == "Введите корректные данные"
+
+
+
+
+
 
 
 
