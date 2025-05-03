0c812f57f1fd55943fe43a919b22fb41d4d8a63f
Igor Chertovskoy <igor.chertovskoy.88.skypro@gmail.com>
2025-05-03 14:02:50 +0300

    add logger

--- src/masks.py
+++ src/masks.py
@@ -1,21 +1,36 @@
 from typing import Union
+import logging
+
+
+masks_logger = logging.getLogger("masks_log")
+masks_logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler('../logs/masks.log', encoding='utf-8')
+file_handler.setFormatter(formatter)
+masks_logger.addHandler(file_handler)
+
 
 """Получаем данные от пользователя"""
-
-
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Возвращаем замаскированый номер карты"""
+    masks_logger.info(f"Получение номера карты: {card_number}")
     len_number = 16
+    masks_logger.info("Проверка из 16 цифр состоит номер карты или нет")
     if len(card_number) == len_number and card_number.isdigit():
+        masks_logger.info("Результат работы функции get_mask_card_number получен")
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     else:
+        masks_logger.error("Вы ввели некоректные данные!")
         return "Вы ввели некоректные данные!"
 
 
 def get_mask_account(account_number: Union[str]) -> Union[str]:
     """Возвращаем замаскированный номер счета"""
     len_acc_number = 20
+    masks_logger.info(f"Получение номера карты: {account_number}")
     if len(account_number) == len_acc_number and account_number.isdigit():
+        masks_logger.info("Результат работы функции get_mask_account получен")
         return f"**{account_number[-4:]}"
     else:
+        masks_logger.error("Вы ввели некоректные данные!")
         return "Вы ввели некоректные данные!"
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,25 @@
 import json
+import logging
+
+masks_logger = logging.getLogger("masks_log")
+masks_logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler('../logs/utils.log', encoding='utf-8')
+file_handler.setFormatter(formatter)
+masks_logger.addHandler(file_handler)
 
 
 def get_transaction_json(operations_file):
     with open(operations_file, encoding="utf-8") as file:
         try:
             operations_data = json.load(file)
+            masks_logger.info("Операция выполнена!")
         except json.JSONDecodeError:
+            masks_logger.error("Ошибка формата!!")
             return []
         except FileNotFoundError:
+            masks_logger.info("Информация отсутствует!")
             return []
     return operations_data
+masks_logger.info("Программа завершила работу!")
+
